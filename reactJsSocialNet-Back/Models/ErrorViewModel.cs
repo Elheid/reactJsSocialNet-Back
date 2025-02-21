7769627bb46c23bf85f20fc0e36c4c0e5840f895
Elheid <Elfieleintsvard@mail.ru>
2025-02-21 19:06:46 +0500

    Добавьте файлы проекта.

---
+++ reactJsSocialNet-Back/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace reactJsSocialNet_Back.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
